892f46bcfe718158a98b87e31d67a9fcfb1f9a6d
multfilm83 <9404147@gmail.com>
2021-12-30 22:31:12 +0300

    new version

--- hw_2_sql_table.sql
+++ hw_2_sql_table.sql
@@ -1,9 +1,10 @@
---SQL_DDL_Joins
---Таблица employees
+--SQL_DDL
+
 
 --Создать таблицу employees
 -- id. serial,  primary key,
 -- employee_name. Varchar(50), not null
+
 create table employees(
 	id serial primary key,
 	employee_name varchar(50) not null
@@ -81,15 +82,20 @@ values (default, 'Mikhail Kerzhakov'),
 	   (default, 'Dmitri Samartsev'),
 	   (default, 'Kirill Stolbov'),
 	   (default, 'Ilya Rukin');
+	   
 	  
+select * from employees;
+
+
 --Таблица salary
 --Создать таблицу salary
 -- id. Serial  primary key,
--- monthly_salary. Int, not null  
+-- monthly_salary. Int, not null
 create table salary(
 	id serial primary key,
 	monthly_salary int not null
 );
+
 
 --Наполнить таблицу salary 15 строками:
 insert into salary(monthly_salary)
@@ -109,12 +115,14 @@ values (1000),
 	   (2300),
 	   (2400),
 	   (2500);
-	  
---Таблица employee_salary
+	   
+select * from salary;
+
 --Создать таблицу employee_salary
 -- id. Serial  primary key,
 -- employee_id. Int, not null, unique
--- salary_id. Int, not null   
+-- salary_id. Int, not null
+
 create table employee_salary(
 	id serial primary key,
 	employee_id int not null unique,
@@ -123,63 +131,67 @@ create table employee_salary(
 
 --Наполнить таблицу employee_salary 40 строками:
 -- в 10 строк из 40 вставить несуществующие employee_id
-insert into employee_salary 
-values (default, 10, 8),
-	   (default, 9, 2),
-	   (default, 8, 3),
-	   (default, 7, 4),
-	   (default, 6, 5),
-	   (default, 5, 6),
-	   (default, 4, 7),
-	   (default, 3, 8),
-	   (default, 2, 9),
-	   (default, 1, 10),
-	   (default, 20, 11),
-	   (default, 19, 12),
-	   (default, 18, 13),
-	   (default, 17, 14),
-	   (default, 16, 15),
-	   (default, 15, 1),
-	   (default, 14, 2),
-	   (default, 13, 3),
-	   (default, 12, 4),
-	   (default, 11, 5),
-	   (default, 30, 6),
-	   (default, 29, 7),
-	   (default, 28, 8),
-	   (default, 27, 9),
-	   (default, 26, 10),
-	   (default, 25, 11),
-	   (default, 24, 12),
-	   (default, 23, 13),
-	   (default, 22, 14),
-	   (default, 21, 15),
-	   (default, 44, 1),
-	   (default, 55, 2),
-	   (default, 66, 3),
-	   (default, 77, 4),
-	   (default, 88, 5),
-	   (default, 99, 6),
-	   (default, 100, 7),
-	   (default, 111, 8),
-	   (default, 122, 9),
-	   (default, 133, 10);
 
---Таблица roles
+insert into employee_salary (id, employee_id, salary_id)
+values (default, 1, 1),
+       (default, 2, 2),
+       (default, 3, 3),
+       (default, 4, 4),
+       (default, 5, 5),
+       (default, 6, 6),
+       (default, 7, 7),
+       (default, 8, 8),
+       (default, 9, 9),
+       (default, 10, 10),
+       (default, 11, 11),
+       (default, 12, 12),
+       (default, 13, 13),
+       (default, 14, 14),
+       (default, 15, 15),
+       (default, 16, 16),
+       (default, 17, 1),
+       (default, 18, 2),
+       (default, 19, 3),
+       (default, 20, 4),
+       (default, 21, 5),
+       (default, 22, 6),
+       (default, 23, 7),
+       (default, 24, 8),
+       (default, 25, 9),
+       (default, 26, 10),
+       (default, 27, 11),
+       (default, 28, 12),
+       (default, 29, 13),
+       (default, 30, 14),
+       (default, 77, 15),
+       (default, 88, 16),
+       (default, 99, 1),
+       (default, 100, 2),
+       (default, 111, 3),
+       (default, 122, 4),
+       (default, 133, 5),
+       (default, 144, 6),
+       (default, 155, 7),
+       (default, 166, 8);
+     
+select * from employee_salary; 
+
 --Создать таблицу roles
 -- id. Serial  primary key,
--- role_name. int, not null, unique	  
+-- role_name. int, not null, unique
+
 create table roles(
 	id serial primary key,
 	role_name int not null unique
 );
 
--- Поменять тип столба role_name с int на varchar(30)
-alter table roles1
+--Поменять тип столба role_name с int на varchar(30)
+
+alter table roles
 alter column role_name type varchar(30);
 
--- Наполнить таблицу roles 20 строками:
-insert into roles1 (id, role_name)
+--Наполнить таблицу roles 20 строками:
+insert into roles (id, role_name)
 values (default, 'Junior Python developer'),
 	   (default, 'Middle Python developer'),
 	   (default, 'Senior Python developer'),
@@ -195,50 +207,70 @@ values (default, 'Junior Python developer'),
 	   (default, 'Project Manager'),
 	   (default, 'Designer'),
 	   (default, 'HR'),
+	   (default, 'CEO'),
 	   (default, 'Sales manager'),
 	   (default, 'Junior Automation QA engineer'),
 	   (default, 'Middle Automation QA engineer'),
-	   (default, 'Senior Automation QA engineer'),
-	   (default, 'CEO');
+	   (default, 'Senior Automation QA engineer');
 
--- Таблица roles_employee
--- Создать таблицу roles_employee
+select * from roles;
+
+--Создать таблицу roles_employee
 -- id. Serial  primary key,
 -- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
--- role_id. Int, not null (внешний ключ для таблицы roles, поле id)  
-create table roles_employee (
+-- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+
+create table roles_employee(
 	id serial primary key,
 	employee_id int not null unique,
 	role_id int not null,
-	foreign key (employee_id)
-		references employees (id),
-	foreign key (role_id)
-		references roles1 (id)
+		foreign key (employee_id)
+			references employees (id),
+		foreign key (role_id)
+			references roles (id)
 );
 
--- Наполнить таблицу roles_employee 40 строками:
-insert into roles_employee
-values (default, 21, 1 ),
-       (default, 9, 11 ),
-       (default, 45, 9 ),
-       (default, 38, 7 ),
-       (default, 65, 1 ),
-       (default, 32, 8 ),
-       (default, 5, 2 ),
-       (default, 13, 12 ),
-       (default, 15, 4 ),
-       (default, 20, 3 ),
-       (default, 25, 6 ),
-       (default, 4, 13 ),
-       (default, 1, 10 ),
-       (default, 16, 14 ),
-       (default, 23, 3 ),
-       (default, 22, 5 ),
-       (default, 12, 7 ),
-       (default, 18, 9 ),
-       (default, 19, 10 ),
-       (default, 29, 13 );
-      
- 
+--Наполнить таблицу roles_employee 40 строками:
+insert into roles_employee (id, employee_id, role_id) 
+values (default, 5, 3),
+	   (default, 7, 5),
+	   (default, 9, 7),
+	   (default, 11, 9),
+	   (default, 13, 11),
+	   (default, 21, 15),
+	   (default, 23, 17),
+	   (default, 27, 19),
+	   (default, 29, 20),
+	   (default, 31, 1),
+	   (default, 34, 2),
+	   (default, 38, 4),
+	   (default, 40, 6),
+	   (default, 42, 8),
+	   (default, 45, 10),
+	   (default, 48, 12),
+	   (default, 51, 14),
+	   (default, 53, 16),
+	   (default, 55, 18),
+	   (default, 56, 13),
+	   (default, 59, 1),
+	   (default, 61, 3),
+	   (default, 63, 5),
+	   (default, 65, 7),
+	   (default, 67, 9),
+	   (default, 69, 11),
+	   (default, 70, 13),
+	   (default, 1, 15),
+	   (default, 2, 17),
+	   (default, 3, 19),
+	   (default, 4, 2),
+	   (default, 6, 4),
+	   (default, 8, 6),
+	   (default, 10, 8),
+	   (default, 12, 10),
+	   (default, 14, 12),
+	   (default, 16, 14),
+	   (default, 18, 16),
+	   (default, 20, 18),
+	   (default, 22, 20);
 
-	  
+select * from roles_employee;
